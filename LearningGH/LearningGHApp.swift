d103bb57abaa872e71c6fc6ade8be2e1a1c39c25
akhmet-tolepov <zhakiya190167@gmail.com>
2024-04-01 14:14:23 +0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Akhmet Tolepov on 01.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
